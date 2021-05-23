b2b89799274cf6fd7a558688333b47bdbd6550b8
Gondrak08 <vitor.alecrim.p@gmail.com>
2021-05-23 14:53:51 -0300

    a/b test implemented

--- src/container/Header/index.js
+++ src/container/Header/index.js
@@ -6,7 +6,48 @@ import PlayButton from '../../assets/Playbutton.svg'
 import HeaderBox from '../../assets/ImageHeader.svg'
 import ProfileImg from '../../assets/ProfileImage.svg'
 
+
+import { Experiment, Variant } from '@marvelapp/react-ab-test'
+
 export const Header = () => {
+
+
+    const HeroTextA = () => {
+        return (
+            <>
+                <div>    
+                    <h1>
+                        Easily creator or join a local nanny share with Hapu
+                    </h1>
+                </div>
+                <div>
+                    <p>Hapu is Airbnb for nanny share. Share you home, nanny and costs and create a new flexbile, affordable solutions in childcare.</p>
+                </div>
+            </>
+        )
+    }
+
+    const HeroTextB = () => {
+        return (
+            <>
+                <div>
+                    <h1>
+                        Create the childcare you need at a price you can afford
+                    </h1>
+                </div>
+                <div>
+                    <p>Connect with other local families to share a nanny from as low as $10.00/hr each. Create your family profile today to get started.</p>
+                </div>
+            </>
+        )
+    }
+
+
+
+
+
+
+
     return (
        
         <header>
@@ -15,14 +56,14 @@ export const Header = () => {
             <div className="header" >
                 <div className="header_container" >
                     <div className="header_info" >
-                        <div>
-                            <h1>
-                                Easily creator or join a local nanny share with Hapu
-                            </h1>
-                        </div>
-                        <div>
-                            <p>Hapu is Airbnb for nanny share. Share you home, nanny and costs and create a new flexbile, affordable solutions in childcare.</p>
-                        </div>
+                            <Experiment name="Hero Text">
+                                <Variant name="original">
+                                    <HeroTextA />
+                                </Variant>
+                                <Variant name="test">
+                                    <HeroTextB />
+                                </Variant>
+                            </Experiment>
                         <div className="play_button_box" >
                             <a href="#">
                                 <img src={PlayButton} alt=""/>
